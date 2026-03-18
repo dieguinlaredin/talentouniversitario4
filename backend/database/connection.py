598b33d8810cc1cd3f6f64f4b008e6f4a8ba50c7
dieguinlaredin <diegolaredo45@gmail.com>
2026-03-17 23:07:03 -0600

    Update connection.py

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,15 +1,19 @@
+import os
 import mysql.connector
-from mysql.connector import Error
 
 def get_connection():
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='JazielSQL',
-            database='talento_universitario'
+            host=os.getenv("MYSQLHOST", "shuttle.proxy.rlwy.net"),
+            port=int(os.getenv("MYSQLPORT", 15913)),
+            user=os.getenv("MYSQLUSER", "root"),
+            password=os.getenv("MYSQLPASSWORD", "yuuIRhHjXpeORsxKmWtwNjlWanAtDPwS"),
+            database=os.getenv("MYSQLDATABASE", "talento_universitario"),
+            autocommit=True
         )
+        print("Conexion exitosa a MySQL")
         return connection
-    except Error as ex:
-        print("Error conectando a la base de datos: ", ex)
+
+    except Exception as ex:
+        print("Error conectando a la base de datos:", ex)
         return None
